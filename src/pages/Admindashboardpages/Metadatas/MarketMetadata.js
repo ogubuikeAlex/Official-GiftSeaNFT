26e1361ff0958824b784b9e4cb714d172cfb967d
Hezhykeyhel <damilolaegbetayo12@gmail.com>
2022-03-04 14:45:32 +0100

    Updated

--- src/pages/Admindashboardpages/Metadatas/MarketMetadata.js
+++ src/pages/Admindashboardpages/Metadatas/MarketMetadata.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 import Dashboard from '../../Userdashboardpages/Dashboard'
 import {Link} from 'react-router-dom'
 import woman from '../../../img/woman.png';
+import MarketMetaStyled from '../../../Styled-components/MetadataStyled'
 
 const Metadata = () => {
     return (
         <div>
-            <MetadataAdjusted>
+            <MarketMetaStyled>
             <div style={{display: 'flex'}}>
             <Link to='../marketplace'><i className='fas fa-arrow-left' style={{marginLeft: '10px', cursor: 'pointer', fontWeight: '600', width: '21px', marginTop:'10px'}}></i>&nbsp; &nbsp; </Link>
             <p style={{fontFamily: 'Inter', fontSize: '24px', fontWeight: '500' }}>Marketplace / NFT Metadata</p>
@@ -48,123 +49,11 @@ const Metadata = () => {
             </div> 
             </div>
             </div>
-            </MetadataAdjusted>
+            </MarketMetaStyled>
             <Dashboard/>
         </div>
 
     )
 }
-
-const MetadataAdjusted = styled.div`
-width: 718.99px;
-@media only screen and (max-width: 1316px){
-    width: 70%;
-    height: fit-content;
-    margin-left:-35px;
-    padding: 1em;
-}
-@media only screen and (max-width: 1165px){
-    margin-left: -45px;
-}
-@media only screen and (max-width: 1120px){
-    margin-left: -55px;
-}
-@media(max-width: 1080px){
-    margin-left: -65px;
-}
-@media only screen and (max-width: 1000px){
-    margin-left: -80px;
-}
-@media only screen and (max-width: 1000px){
-    margin-left: -80px;
-}
-@media(max-width: 918px){
-    margin-left: -85px;
-}
-@media(max-width: 900px){
-    margin-left: -95px;
-}
-@media(max-width: 849px){
-    width: 100%;
-    transform: translateX(40px);
-}
-@media only screen and (max-width: 768px){
-    transform: translateX(90px);       
-}
-@media(max-width: 540px){
-    width: 100%;
-}
-.metadata_container{
-    width: 100%;
-    background: #fff;
-    padding: 0.8em;
-    margin-top: 15px;
-    object-fit: cover;
-    img{
-        height: 316px;
-        width: 100%;
-        object-fit: cover;
-    }
-}
-.nftContainer{
-    margin-top: 8px;
-    margin-bottom: 8px;
-    .listedNFT{
-        font-family: Inter;
-        font-size: 24px;
-        font-weight: 500;
-        line-height: 29px;
-        text-align: left;
-    }
-    .Buy{
-        border: none;
-        color: #fff;
-        padding: 0.8em;
-        background: #02AAB0;
-        border-radius: 3px;
-        width: 154px;
-    }
-}
-.gridContainer{
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    position: relative;
-    div{
-        border: 1px solid rgba(26, 16, 54, 0.4);
-        padding: 0.5em;
-        justify-content: center;
-        text-align: left;
-        line-height: 10px;
-        margin-top: 10px;
-        .title{
-            font-family: Inter;
-            font-size: 12px;
-            font-weight: 500;
-            line-height: 15px;
-            text-align: center;
-            color: #645D66;
-        }
-        .details{
-            font-family: Inter;
-            font-size: 18px;
-            font-weight: 700;
-            line-height: 30px;
-            text-align: center;
-        }
-    }
-}
-        .contents{
-        .description{
-            font-family: Inter;
-            margin-top: 10px;
-            font-size: 18px;
-            font-weight: 500;
-            line-height: 22px;
-            text-align: left;
-    
-        }
-    }
-
-`;
 
 export default Metadata;
